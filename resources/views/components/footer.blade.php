105d1db9de2f48ac6bc2970f59355cd7e7fe0971
Thaw Htoo Zin <thawhtoozin@Thaws-Mac-mini.local>
2025-03-28 18:16:46 +0630

    fixed footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="bg-primary text-white px-6 py-6">
-    <div class="grid grid-cols-5 text-center items-center border-b border-gray-500 pb-4">
+    <div class="grid grid-cols-1 md:grid-cols-5 text-center items-center border-b border-gray-500 pb-4 gap-4 md:gap-0">
 
         <!-- Left Column: Social Icons -->
-        <div class="flex space-x-4 justify-center items-center col-span-2">
+        <div class="flex space-x-4 justify-center items-center col-span-1 md:col-span-2">
             <a href="https://facebook.com" target="_blank"
                 class="text-white hover:text-gray-300 transform transition-transform duration-200 hover:scale-110">
                 <i class="fab fa-facebook-f text-2xl"></i>
@@ -22,13 +22,13 @@
         </div>
 
         <!-- Center Column: Logo -->
-        <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center col-span-1">
             <h1 class="text-2xl font-semibold mb-2">ASOKA</h1>
             <img src="/images/logo.png" alt="Asoka Logo" class="w-16 md:w-24 inline drop-shadow-lg">
         </div>
 
         <!-- Right Column: Links -->
-        <div class="flex flex-col items-center col-span-2 space-y-1">
+        <div class="flex flex-col items-center col-span-1 md:col-span-2 space-y-1">
             <a href="/whoweare"
                 class="text-white hover:text-gray-300 text-sm transition-colors duration-200">@lang('footer.whoweare')</a>
             <a href="/courses"
@@ -46,7 +46,8 @@
 
     <!-- Footer Bottom Bar -->
     <div class="flex justify-center items-center pt-4">
-        <p class="text-xs text-gray-400">&copy; 2024 Asoka Center for Buddhist Studies. All rights reserved. Powered By
+        <p class="text-xs text-gray-400 text-center">&copy; 2024 Asoka Center for Buddhist Studies. All rights reserved.
+            Powered By
             <a href="https://protechmm.com/" class="hover:underline">ProTechMM</a>
         </p>
     </div>
